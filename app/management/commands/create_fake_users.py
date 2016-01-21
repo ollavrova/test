313d7ee1cc46b93fc6ecc42cb842fc0639ee6a1a
krocozabr <krocozabr@gmail.com>
2016-01-21 18:05:58 +0200

    added command

---
+++ app/management/commands/create_fake_users.py
@@ -0,0 +1,37 @@
+import datetime
+from optparse import make_option
+from django.contrib.auth.models import User
+import requests
+from django.core.management import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    help = 'create a fake users number N'
+    option_list = BaseCommand.option_list + (
+        make_option(
+            "-n",
+            "--number",
+            dest="number",
+            help="specify number of users",
+            metavar="NUM"
+        ),
+    )
+
+    def handle(self, *args, **options):
+        if options['number'] == None :
+                raise CommandError("Option `--number=...` must be specified.")
+        n = options['number']
+        r = requests.get('https://randomuser.me/api/?results='+n)
+        if r.status_code != 200:
+             self.stderr.writeself.stdout.write('Error! Wrong response on request: status_code='+r.status_code)
+        users = r.json()['results']
+        for user in users:
+            User.objects.create_user(
+                username=user['user']['username'],
+                first_name=user['user']['name']['first'],
+                last_name=user['user']['name']['last'],
+                email=user['user']['email'],
+                password=user['user']['password'],
+                date_joined=datetime.datetime.today()
+            )
+        self.stdout.write('Created '+n+' users.')
