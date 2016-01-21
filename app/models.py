b26a78245b2b86f33cbf6bf2e97cb22949e25e87
krocozabr <krocozabr@gmail.com>
2016-01-22 01:11:12 +0200

    added migrations and link table

--- app/models.py
+++ app/models.py
@@ -1,5 +1,9 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Shorted(models.Model):
+    long_url = models.CharField(max_length=500)
+    short_url = models.CharField(max_length=100)
+    user = models.ForeignKey(User)
